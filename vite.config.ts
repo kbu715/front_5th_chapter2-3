1ab0afcd9ac71ebe8df132fe2ef2c55e0ed6e9ec
Paul Kang <kbu715@gmail.com>
2025-05-03 00:20:29 +0900

    chore: 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
+
+const mode = process.env.NODE_ENV || "development"
+const env = loadEnv(mode, process.cwd(), "")
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: env.VITE_BASE_PATH,
   plugins: [react()],
   server: {
     proxy: {
