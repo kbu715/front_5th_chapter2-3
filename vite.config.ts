cd7fbeb539ec12bb09d541e264f6f248cc68b4a1
Paul Kang <kbu715@gmail.com>
2025-05-03 23:49:51 +0900

    chore: replace cdn tailwind with Tailwind CSS vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite"
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 
 const mode = process.env.NODE_ENV || "development"
@@ -7,7 +8,7 @@ const env = loadEnv(mode, process.cwd(), "")
 // https://vite.dev/config/
 export default defineConfig({
   base: env.VITE_BASE_PATH,
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
