a7fa103d0bdeb081faf180b9a13dbeb328d7da57
Paul Kang <kbu715@gmail.com>
2025-05-03 23:03:02 +0900

    refactor: PostTagFilter 역할 분리

--- src/features/post/ui/PostController.tsx
+++ src/features/post/ui/PostController.tsx
@@ -1,4 +1,3 @@
-import { usePostTagsQuery } from "../../../entities/post/model/hooks/queries"
 import { usePostQueryParams } from "../model/hooks"
 import { PostSearchInput } from "./PostSearchInput"
 import { PostSortBySelect } from "./PostSortBySelect"
@@ -9,12 +8,11 @@ export const PostController = () => {
   const { params, setters } = usePostQueryParams()
   const { sortBy, sortOrder, tag } = params
   const { setSortBy, setSortOrder, setTag } = setters
-  const { data: tags } = usePostTagsQuery()
 
   return (
     <div className="flex gap-4">
       <PostSearchInput />
-      <PostTagFilter selectedTag={tag} onSelectTag={setTag} tags={tags || []} />
+      <PostTagFilter selectedTag={tag} onSelectTag={setTag} />
       <PostSortBySelect value={sortBy} onChange={setSortBy} />
       <PostSortSelect value={sortOrder} onChange={(value) => setSortOrder(value as "asc" | "desc")} />
     </div>
--- src/features/post/ui/PostTagFilter.tsx
+++ src/features/post/ui/PostTagFilter.tsx
@@ -1,24 +1,26 @@
-import { Tag } from "../../../entities/post/model/types"
+import { usePostTagsQuery } from "../../../entities/post/model/hooks/queries"
 import { Select } from "../../../shared/ui"
 
 interface PostTagFilterProps {
   selectedTag: string
   onSelectTag: (tag: string) => void
-  tags: Tag[]
 }
 
-export const PostTagFilter = ({ selectedTag, onSelectTag, tags }: PostTagFilterProps) => (
-  <Select value={selectedTag} onValueChange={onSelectTag}>
-    <Select.Trigger className="w-[180px]">
-      <Select.Value placeholder="태그 선택" />
-    </Select.Trigger>
-    <Select.Content>
-      <Select.Item value="all">모든 태그</Select.Item>
-      {tags.map((tag) => (
-        <Select.Item key={tag.url} value={tag.slug}>
-          {tag.slug}
-        </Select.Item>
-      ))}
-    </Select.Content>
-  </Select>
-)
+export const PostTagFilter = ({ selectedTag, onSelectTag }: PostTagFilterProps) => {
+  const { data: tags } = usePostTagsQuery()
+  return (
+    <Select value={selectedTag} onValueChange={onSelectTag}>
+      <Select.Trigger className="w-[180px]">
+        <Select.Value placeholder="태그 선택" />
+      </Select.Trigger>
+      <Select.Content>
+        <Select.Item value="all">모든 태그</Select.Item>
+        {tags?.map((tag) => (
+          <Select.Item key={tag.url} value={tag.slug}>
+            {tag.slug}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select>
+  )
+}
