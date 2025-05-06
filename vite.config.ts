4a45c592e8fc3007f940c9741f76528f1734f4f8
Paul Kang <kbu715@gmail.com>
2025-05-06 15:35:07 +0900

    chore: 상대경로 "@" alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 const mode = process.env.NODE_ENV || "development"
 const env = loadEnv(mode, process.cwd(), "")
@@ -9,6 +10,16 @@ const env = loadEnv(mode, process.cwd(), "")
 export default defineConfig({
   base: env.VITE_BASE_PATH,
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@app": path.resolve(__dirname, "src/app"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,19 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@app": path.resolve(__dirname, "src/app"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
